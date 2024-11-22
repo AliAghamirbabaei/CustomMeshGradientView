f88d7c80d70ac79994254262d73557b3aad88e13
Ali Aghamirbabaei <Ali.Aghamirbabaei@gmail.com>
2024-11-22 17:36:11 +0330

    Initial Commit

---
+++ MeshGradient/MeshGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshGradientApp.swift
+//  MeshGradient
+//
+//  Created by Ali Aghamirbabaei on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MeshGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
